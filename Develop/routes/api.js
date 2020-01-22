19364e332e2943f3ec0b0a4c9e71b9d704c40619
dsorthep <detvisiths@yahoo.com>
2020-01-21 23:00:05 -0600

    i'm tired

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -48,4 +48,15 @@ router.get("/api/workout", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", function (req, res){
+    console.log(req.body)
+    db.Workout.where('_id', req.params.id).update({$push: {"exercises": req.body}})
+    .then(function(results){
+        res.json(results)
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
+});
+
 module.exports = router;
